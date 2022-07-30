29e7f178e2a4785f18c8ab816a02edefc75c3661
janae.wel <janae.wel@gmail.com>
2022-07-30 13:46:20 -0400

    Manager object created

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,7 @@
 const Manager = require('../lib/Manager');
 
 test('create Manager object', () => {
-    const manager = new Manager('Michael');
+    const manager = new Manager('Michael', 8144798957028, 'michaelscott@gmail.com', 234-567-8912);
 
     expect(manager.phone).toEqual(expect.any(Number));
 });
