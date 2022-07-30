a0a39da99641842487276c3d7e145c4048609883
janae.wel <janae.wel@gmail.com>
2022-07-30 12:28:06 -0400

    fixing another Employee test bug

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,9 +3,10 @@ const Employee = require('../lib/Employee');
 jest.mock('../lib/__mocks__/Employee');
 
 test('create an Employee', () => {
-    const employee = new Employee('Jim');
+    const employee = new Employee('Jim', 8921478933, 'jimhalp@gmail.com');
+
 
     expect(employee.name).toBe('Jim');
     expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toBe(expect.stringContaining('@'));
+    expect(employee.email).toBe(expect.any(String));
 });
