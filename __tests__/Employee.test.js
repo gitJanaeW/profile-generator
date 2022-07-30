c92fd161001f6d54dd3b66030ac38d5772b90e57
janae.wel <janae.wel@gmail.com>
2022-07-30 13:11:17 -0400

    fixing Manager test bug

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,7 @@ const Employee = require('../lib/Employee');
 
 test('create an Employee', () => {
     const employee = new Employee('Jim', 8921478933, 'jimhalp@gmail.com');
-    expect(employee.name).toBe('Jim');
+    expect(employee.employeeName).toBe('Jim');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.stringContaining('@'));
 });
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,7 @@
 const Manager = require('../lib/Manager');
+
+test('create Manager object', () => {
+    const manager = new Manager('Michael');
+
+    expect(manager.phone).toEqual(expect.any(Number));
+});
