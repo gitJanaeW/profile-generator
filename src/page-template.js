e6bf626cd3018a5f728fd284abcc892cfbba5182
janae.wel <janae.wel@gmail.com>
2022-08-06 21:10:27 -0400

    all done

--- src/page-template.js
+++ src/page-template.js
@@ -60,14 +60,13 @@ const generateTemplate = teamArray => {
             </section>
             `;
         }
-        teamHtml += `
+    };
+    teamHtml += `
         </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
         </body>
         </html>
-        `;
-        return teamHtml;
-    };
+    `;
     return teamHtml;
 }
 
